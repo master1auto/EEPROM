a88d276aacb72ce7d4ef23d3f27b602a6ad38d7e
maissatem <maissatem1994@gmail.com>
2015-12-26 00:31:02 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,40 +1,25 @@
-main()
+#include <stdio.h>
+void writeEEPROM(unsigned char address, unsigned char data)
 {
-  char kp;
-  char kd;
-  char ki;
-
-  EEADR=10;
-  EEDAT=kp;
-
+  EEADR=address;
+  EEDAT=data;
   EECON1.EEPGD = 0; //Selecting EEPROM Data Memory
   EECON1.WREN = 1; //Enable writing of EEPROM
   INTCON=0; //Diables the interrupt
   EECON2=0x55; //Required sequence for write to internal EEPROM
   EECON2=0xAA; //Required sequence for write to internal EEPROM
   EECON1.WR = 1; //Initialise write cycle
-  
-  EEADR=11;
-  EEDAT=kd;
-
-  EECON1.EEPGD = 0; //Selecting EEPROM Data Memory
-  EECON1.WREN = 1; //Enable writing of EEPROM
-  INTCON=0; //Diables the interrupt
-  EECON2=0x55; //Required sequence for write to internal EEPROM
-  EECON2=0xAA; //Required sequence for write to internal EEPROM
-  EECON1.WR = 1; //Initialise write cycle
-  
-  EEADR=12;
-  EEDAT=ki;
-
-  EECON1.EEPGD = 0; //Selecting EEPROM Data Memory
-  EECON1.WREN = 1; //Enable writing of EEPROM
-  INTCON=0; //Diables the interrupt
-  EECON2=0x55; //Required sequence for write to internal EEPROM
-  EECON2=0xAA; //Required sequence for write to internal EEPROM
-  EECON1.WR = 1; //Initialise write cycle
-  
-  
+  while(EECON1.WR==1)
+  {
+    asm nop (); // wait for write to complete
+  }
   INTCON.GIE = 1;//Enables Interrupt
   EECON1.WREN = 0; //To disable write
 }
+//----------------------------------//
+main()
+{
+  writeEEPROM(unsigned char 10, unsigned char kp)
+  writeEEPROM(unsigned char 11, unsigned char kd)
+  writeEEPROM(unsigned char 12, unsigned char ki)
+  }
